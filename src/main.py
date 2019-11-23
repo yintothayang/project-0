d3c1ae75dbe96f17887c27462fcbe6bcbfd35720
yin <yintothayang@gmail.com>
2019-11-23 10:27:42 -0800

    looking good

--- src/main.py
+++ src/main.py
@@ -15,10 +15,10 @@ import sys
 import os
 
 EPOCHS = 10
-BATCH_SIZE = 128
-STEPS_PER_EPOCH = 33
-IMG_WIDTH = 150
-IMG_HEIGHT = IMG_WIDTH
+BATCH_SIZE = 32
+STEPS_PER_EPOCH = 32
+IMG_WIDTH = 440
+IMG_HEIGHT = 248
 CLASS_NAMES = []
 
 class CollectBatchStats(tf.keras.callbacks.Callback):
@@ -75,84 +75,33 @@ def verify(image_batch, label_batch, labels_by_key):
 
 
 def train(train_data_gen, validate_data_gen):
-  model = models.Sequential([
-    layers.Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
-    layers.MaxPooling2D(),
-    layers.Conv2D(32, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Flatten(),
-    layers.Dense(512, activation='relu'),
-    layers.Dense(1, activation='sigmoid')
-  ])
-
+  model = models.Sequential()
+  model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)))
+  model.add(layers.MaxPooling2D((2, 2)))
+  model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+  model.add(layers.MaxPooling2D((2, 2)))
+  model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+  model.add(layers.Flatten())
+  model.add(layers.Dense(64, activation='relu'))
+  model.add(layers.Dense(4, activation='softmax'))
 
   model.compile(optimizer='adam',
-                loss='binary_crossentropy',
+                loss='sparse_categorical_crossentropy',
                 metrics=['accuracy'])
 
-  model.summary()
+  history = model.fit(train_data_gen,
+                      epochs=5,
+                      validation_data=validate_data_gen)
 
-
-  history = model.fit_generator(
-    train_data_gen,
-    steps_per_epoch=len(train_data_gen.filepaths) / BATCH_SIZE,
-    epochs=EPOCHS,
-    validation_data=validate_data_gen,
-    validation_steps=len(validate_data_gen.filepaths) / BATCH_SIZE
-  )
-
-
-
-  acc = history.history['accuracy']
-  val_acc = history.history['val_accuracy']
-
-  loss = history.history['loss']
-  val_loss = history.history['val_loss']
-
-  epochs_range = range(EPOCHS)
-
-  plt.figure(figsize=(8, 8))
-  plt.subplot(1, 2, 1)
-  plt.plot(epochs_range, acc, label='Training Accuracy')
-  plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+  plt.plot(history.history['accuracy'], label='accuracy')
+  plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+  plt.xlabel('Epoch')
+  plt.ylabel('Accuracy')
+  plt.ylim([0.5, 1])
   plt.legend(loc='lower right')
-  plt.title('Training and Validation Accuracy')
-
-  plt.subplot(1, 2, 2)
-  plt.plot(epochs_range, loss, label='Training Loss')
-  plt.plot(epochs_range, val_loss, label='Validation Loss')
-  plt.legend(loc='upper right')
-  plt.title('Training and Validation Loss')
   plt.show()
 
-
-  # class_names = sorted(train_data_gen.class_indices.items(), key=lambda pair:pair[1])
-  # class_names = np.array([key.title() for key, value in class_names])
-  # print(class_names)
-
-  # image_batch, label_batch = next(validate_data_gen)
-  # predicted_batch = model.predict(image_batch)
-  # predicted_id = np.argmax(predicted_batch, axis=-1)
-  # predicted_label_batch = class_names[predicted_id]
-  # label_id = np.argmax(label_batch, axis=-1)
-
-  # plt.figure(figsize=(10,9))
-  # plt.subplots_adjust(hspace=0.5)
-
-  # print(predicted_label_batch)
-  # print(label_id)
-
-  # for n in range(30):
-  #   plt.subplot(6,5,n+1)
-  #   plt.imshow(image_batch[n])
-  #   color = "green" if predicted_id[n] == label_id[n] else "red"
-  #   plt.title(predicted_label_batch[n].title(), color=color)
-  #   plt.axis('off')
-  # _ = plt.suptitle("Model predictions (green: correct, red: incorrect)")
-
-  # plt.show()
+  # test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
 
 def main():
@@ -161,8 +110,9 @@ def main():
   image_batch, label_batch = next(train_data_gen)
 
   labels_by_key = {v: k for k, v in train_data_gen.class_indices.items()}
+
   # print(image_batch.shape)
-  # verify(image_batch, label_batch, labels_by_key)
+  verify(image_batch, label_batch, labels_by_key)
   # print(len(train_data_gen.filepaths))
   train(train_data_gen, validate_data_gen)
 
